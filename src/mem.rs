80cae5292d9ae7b4a385cfe81b05cea16c344c8c
mattngaw <mattngaw2002@gmail.com>
2022-05-15 21:12:28 -0400

    Refactor memory implementation

--- src/mem.rs
+++ src/mem.rs
@@ -1,29 +1,35 @@
+use crate::reg::Reg;
+
 pub struct Mem {
-    pub data: [u8; 0x10000]
+    data: [u8; 0x10000],
+    mar: Reg,
+    mdr: Reg
 }
 
 impl Mem {
-    pub fn new() -> Self {
+    pub fn new(mar: Reg, mdr: Reg) -> Self {
         Mem {
-            data: [0x00; 0x10000]
+            data: [0x00; 0x10000],
+            mar: mar,
+            mdr: mdr
         }
     }
 
-    pub fn read(&self, addr: u16) -> u16 {
-        let addr = addr & !(1u16);
+    pub fn read(&mut self) -> () {
+        let addr = self.mar.read() & !(1u16);
         let upper_addr = addr;
         let lower_addr = addr + 1;
         let upper = self.data[upper_addr as usize] as u16;
         let lower = self.data[lower_addr as usize] as u16;
-        (upper << 8) | (lower)
+        self.mdr.write((upper << 8) | (lower));
     }
 
-    pub fn write(&mut self, addr: u16, data: u16) -> () {
-        let addr = addr & !(1u16);
+    pub fn write(&mut self) -> () {
+        let addr = self.mar.read() & !(1u16);
         let upper_addr = addr;
         let lower_addr = addr + 1;
-        let upper_data = ((data & 0xFF00) >> 8) as u8;
-        let lower_data = (data & 0x00FF) as u8;
+        let upper_data = ((self.mdr.read() & 0xFF00u16) >> 8) as u8;
+        let lower_data = (self.mdr.read() & 0x00FFu16) as u8;
         self.data[upper_addr as usize] = upper_data;
         self.data[lower_addr as usize] = lower_data;
     }
@@ -31,37 +37,52 @@ impl Mem {
 
 mod tests {
     use super::Mem;
+    use crate::reg::Reg;
 
     #[test]
     fn test_mem_new() {
-        let mem = Mem::new();
+        let mar = Reg::new();
+        let mdr = Reg::new();
+        let mem = Mem::new(mar, mdr);
         assert_eq!(mem.data, [0x00u8; 0x10000]);
     }
 
     #[test]
     fn test_mem_read() {
-        let mem = Mem::new();
-        assert_eq!(mem.read(0x0000u16), 0x0000);
-        assert_eq!(mem.read(0x0001u16), 0x0000);
+        let mar = Reg::new();
+        let mdr = Reg::new();
+        let mut mem = Mem::new(mar.clone(), mdr.clone());
+        mem.read();
+        assert_eq!(mdr.read(), 0x0000);
+        mar.write(0x0001u16);
+        mem.read();
+        assert_eq!(mdr.read(), 0x0000);
     }
 
     #[test]
     fn test_mem_write() {
-        let mut mem = Mem::new();
-        mem.write(0x0000, 0x0102);
+        let mar = Reg::new();
+        let mdr = Reg::new();
+        mdr.write(0x0102u16);
+        let mut mem = Mem::new(mar.clone(), mdr.clone());
+        mem.write();
         assert_eq!(mem.data[0x0000 as usize], 0x01);
         assert_eq!(mem.data[0x0001 as usize], 0x02);
     }
 
     #[test]
     fn test_mem_read_and_write() {
-        let mut mem = Mem::new();
-        mem.write(0x0000, 0x0102);
-        assert_eq!(mem.read(0x0000u16), 0x0102);
-        assert_eq!(mem.read(0x0001u16), 0x0102);
-        assert_eq!(mem.read(0x0002u16), 0x0000);
-        assert_eq!(mem.read(0x0003u16), 0x0000);
-        mem.write(0xFFFF, 0x1234);
-        assert_eq!(mem.read(0xFFFE), 0x1234);
+        let mar = Reg::new();
+        let mdr = Reg::new();
+        mdr.write(0x0102u16);
+        let mut mem = Mem::new(mar.clone(), mdr.clone());
+        mem.write();
+        mem.read();
+        assert_eq!(mdr.read(), 0x0102);
+        mar.write(0xFFFEu16);
+        mdr.write(0x1234);
+        mem.write();
+        mem.read();
+        assert_eq!(mdr.read(), 0x1234);
     }
 }
